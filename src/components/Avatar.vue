4437c338fcd5fb3ca0a978bbf44301082738c3e5
TieStone <416138794@qq.com>
2023-07-10 16:23:09 +0800

    添加头像的 hover 动画效果

--- src/components/Avatar.vue
+++ src/components/Avatar.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="avatar-container">
-        <el-avatar :size="avatarSize" :src="avatarUrl" :alt="altText"></el-avatar>
+<!--        <el-avatar :size="avatarSize" :src="avatarUrl" :alt="altText"></el-avatar>-->
+        <img :src="avatarUrl" :alt="altText" :width="avatarSize" :height="avatarSize">
     </div>
 </template>
 
@@ -38,4 +39,15 @@ export default {
     height: auto;
     text-align: center;
 }
+
+img {
+    border-radius: 50%;
+    box-shadow: 0 0 11px #888888;
+    transition: box-shadow 0.5s ease-out 100ms;
+}
+
+img:hover {
+    box-shadow: 0 0 50px #97A0ED;
+}
+
 </style>
