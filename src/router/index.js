cde55540e974a9d1505afdab1bfbcb1ab261a5e7
TieStone <416138794@qq.com>
2023-12-24 20:45:55 +0800

    添加tool-box页面

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,17 @@
-import Home from '../views/Home.vue'
-import Love from '../views/Love.vue'
+import Home from '../views/home.vue'
+import Love from '../views/love.vue'
+import ToolBox from '../views/tool-box.vue'
 import {createRouter, createWebHistory} from "vue-router";
 
 const routes = [
     {path: '/', name: Home, component: Home},
     {path: '/love', name: Love, component: Love},
+    {path: '/tool-box', name: ToolBox, component: ToolBox},
 ]
 
 const router = createRouter({
     history: createWebHistory(),
-    routes,
+    routes: routes,
 });
 
 export default router;
