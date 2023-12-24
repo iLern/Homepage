6cf1cc870df532d57a4139b88f8dfd0ec28c2bcf
TieStone <416138794@qq.com>
2023-12-24 19:52:54 +0800

    添加love页面

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,10 @@
 import Home from '../views/Home.vue'
+import Love from '../views/Love.vue'
 import {createRouter, createWebHistory} from "vue-router";
 
 const routes = [
     {path: '/', name: Home, component: Home},
+    {path: '/love', name: Love, component: Love},
 ]
 
 const router = createRouter({
---
+++ src/views/Love.vue
@@ -0,0 +1,75 @@
+<script>
+    export default {
+        name: "Love",
+        components: {
+
+        }
+    }
+</script>
+
+<template>
+    <div id="frame">
+        <div class="heart left"></div>
+        <div class="heart right"></div>
+        <div class="heart bottom"></div>
+    </div>
+</template>
+
+<style scoped>
+* {
+    padding: 0;
+    margin: 0;
+}
+body {
+    background-color: pink;
+}
+#frame {
+    position: relative;
+    width: 400px;
+    height: 300px;
+    margin: 250px auto;
+}
+.left,
+.right {
+    top: 0;
+    width: 200px;
+    height: 200px;
+    border-radius: 50%;
+}
+.left {
+    left: 35px;
+    z-index: 2;
+}
+.right {
+    right: 35px;
+    z-index: 1;
+}
+.bottom {
+    bottom: 36px;
+    left: 100px;
+    width: 200px;
+    height: 200px;
+    transform: rotate(45deg);
+    z-index: 0;
+}
+.heart {
+    position: absolute;
+    box-shadow: 0 0 40px #d5093c;
+    animation: beat 0.8s ease infinite normal;
+    background: linear-gradient(-90deg, #f50a45 0%, #d5093c 40%);
+}
+@keyframes beat {
+    0% {
+        transform: scale(1) rotate(225deg);
+        box-shadow: 0 0 40px #d5093c;
+    }
+    50% {
+        transform: scale(1.1) rotate(225deg);
+        box-shadow: 0 0 70px #d5093c;
+    }
+    100% {
+        transform: scale(1) rotate(225deg);
+        box-shadow: 0 0 40px #d5093c;
+    }
+}
+</style>
