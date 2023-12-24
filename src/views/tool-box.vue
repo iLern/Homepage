74261559fe24513f8b43ed5fd2073f7babbf0b24
TieStone <416138794@qq.com>
2023-12-24 20:45:38 +0800

    添加tool-box页面

---
+++ src/views/tool-box.vue
@@ -0,0 +1,30 @@
+<script>
+    export default {
+        name: "tool-box",
+        components: {
+        },
+    }
+</script>
+
+<template>
+   <h1 class="blink-animation">Editing...</h1>
+</template>
+
+<style scoped>
+@keyframes blink {
+    0% {
+        opacity: 1;
+    }
+    50% {
+        opacity: 0;
+    }
+    100% {
+        opacity: 1;
+    }
+}
+
+.blink-animation {
+    color: #181818;
+    animation: blink 1s ease-in-out infinite; /* 使用 animation 属性应用动画 */
+}
+</style>
